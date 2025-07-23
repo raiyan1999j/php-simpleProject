0e7c839f667e0ce0eefaf04258234134f59939eb
Raiyan <boomboomraiyan@gmail.com>
2025-07-23 11:52:10 +0600

    footer mobile view ui

--- view/pertial/footer.view.php
+++ view/pertial/footer.view.php
@@ -1,14 +1,14 @@
 <!-- footerArea-start -->
- <footer class="mt-[180px] bg-black text-white py-[119px] px-[100px]">
-    <div class="grid grid-cols-5">
-        <div class="col-span-2">
+ <footer class="mt-[180px] bg-black text-white py-[119px] 2xl:px-[100px] xl:px-[50px] px-10">
+    <div class="grid 2xl:grid-cols-5 grid-cols-1 gap-y-10">
+        <div class="xl:col-span-2">
             <div>
                 <h2 class="poppins font-black text-[40px] uppercase">
                     fashion
                 </h2>
             </div>
 
-            <div class="w-[415px] my-8">
+            <div class="xl:w-[415px] w-full my-8">
                 <p class="roboto capitalize text-[#8E8E8E] font-normal text-2xl leading-10">
                     complete your style with awesome clothes from us
                 </p>
@@ -23,10 +23,10 @@
             </div>
         </div>
 
-        <div class="col-span-3 flex flex-row justify-center gap-x-[190px]">
+        <div class="xl:col-span-3 flex xl:flex-row flex-col justify-center items-center gap-x-[190px] gap-y-10">
             <?php foreach($footerMenus as $items):?>
                 <div>
-                    <h4 class="roboto text-[#D9D9D9] font-normal text-2xl capitalize">
+                    <h4 class="roboto text-[#D9D9D9] font-normal text-2xl capitalize xl:text-left text-center">
                         <?= $items["title"]?>
                     </h4>
                     <div class="flex flex-col gap-y-8 mt-8">
